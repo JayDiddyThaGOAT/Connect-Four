978d6ecfd5830b58871e65e6483a78fe05a091c3
JayDiddyThaGOAT <jalenjacksoncs@gmail.com>
2020-12-06 03:02:47 -0800

    Programmed Connect 4 Discs to Drop In Game Board

---
+++ Assets/Scripts/Connect4Board.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Connect4Board : MonoBehaviour
+{
+    public Connect4Disc blackDisc, whiteDisc;
+
+    public int currentPlayer = 1;
+
+    public float rotateDuration = 0.25f;
+
+    private int[,] boardMatrix = new int[6, 7];
+
+    private Connect4Disc currentDisc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetBoard();
+
+        if (currentPlayer == 1)
+        {
+            currentDisc = Instantiate<Connect4Disc>(blackDisc, transform);
+            transform.rotation = Quaternion.identity;
+        }
+        else if (currentPlayer == -1)
+        {
+            currentDisc = Instantiate<Connect4Disc>(whiteDisc, transform);
+            transform.rotation = Quaternion.Euler(0.0f, 180.0f, 0.0f);
+        }
+    }
+
+    void ResetBoard()
+    {
+        for (int row = 0; row < 6; row++)
+        {
+            for (int col = 0; col < 7; col++)
+            {
+                boardMatrix[row, col] = 0;
+            }
+        }
+
+        GameObject [] blackDiscs = GameObject.FindGameObjectsWithTag("Black Disc");
+        foreach (GameObject disc in blackDiscs)
+            Destroy(disc);
+
+        GameObject [] whiteDiscs = GameObject.FindGameObjectsWithTag("White Disc");
+        foreach (GameObject disc in whiteDiscs)
+            Destroy(disc);
+    }
+
+    public float GetXAt(int col, int player)
+    {
+        if (player == 1)
+            return (5 * col) - 15;
+        
+        return (5 * (6 - col)) - 15;
+    }
+
+    public float GetYAt(int row)
+    {
+        return 10 - (5 * row);
+    }
+
+    public int GetColAt(float x, int player)
+    {
+        if (player == 1)
+            return ((int)x + 15) / 5;
+            
+        return 6 - ((int)x + 15) / 5;
+    }
+
+    public int GetRowAt(float y)
+    {
+        return ((int)y - 10) / -5;
+    }
+
+    public int GetRowAvailableAt(int col)
+    {
+        if (boardMatrix[0, col] != 0)
+            return -1;
+
+        int targetRow = 5;
+        while (boardMatrix[targetRow, col] != 0 && targetRow > 0)
+            targetRow--;
+        
+        return targetRow;
+    }
+
+    public void PlaceDiscOn(int player, int row, int col)
+    {
+        boardMatrix[row, col] = player;
+    }
+
+    public IEnumerator Rotate()
+    {
+        Quaternion startRotation = transform.rotation;
+        Quaternion targetRotation = startRotation * Quaternion.Euler(0.0f, -180.0f, 0.0f);
+
+        float t = 0;
+        while (t <= rotateDuration)
+        {
+            t += Time.deltaTime;
+            transform.rotation = Quaternion.Slerp(startRotation, targetRotation, t / rotateDuration);
+
+            yield return null;
+        }
+
+        transform.rotation = targetRotation;
+        yield return StartCoroutine(ChangeTurn());
+    }
+
+    public IEnumerator ChangeTurn()
+    {
+        currentPlayer *= -1;
+
+        yield return 0;
+
+        if (currentPlayer == 1)
+            currentDisc = Instantiate<Connect4Disc>(blackDisc, transform);
+        else if (currentPlayer == -1)
+            currentDisc = Instantiate<Connect4Disc>(whiteDisc, transform);
+    }
+}
---
+++ Assets/Scripts/Connect4Disc.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Connect4Disc : MonoBehaviour
+{
+    public int color = 1;
+
+    public float shiftDuration = 0.25f;
+    public float dropDuration = 0.5f;
+
+    private bool isAboveBoard = true;
+    public bool isPlayer = true;
+
+    private Vector3 touchDownPosition, touchUpPosition;
+
+    private bool isMoving = false;
+
+    private Connect4Board gameBoard;
+    private int currentRow, currentCol;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameBoard = FindObjectOfType<Connect4Board>();
+        currentRow = -1;
+        currentCol = gameBoard.GetColAt(transform.position.x, color);
+    }
+
+    IEnumerator ShiftToColumn(int targetCol)
+    {
+        float targetX = gameBoard.GetXAt(targetCol, color);
+
+        if (Mathf.Abs(targetX) <= 15)
+        {
+            isMoving = true;
+            float startX = gameBoard.GetXAt(currentCol, color);
+
+            float t = 0;
+            while (t <= shiftDuration)
+            {
+                t += Time.deltaTime;
+                float currentX = Mathf.Lerp(startX, targetX, t / shiftDuration);
+                transform.position = new Vector3(currentX, transform.position.y, transform.position.z);
+
+                yield return null;
+            }
+
+            transform.position = new Vector3(targetX, transform.position.y, transform.position.z);
+            currentCol = targetCol;
+            Debug.Log(currentCol);
+
+            isMoving = false;
+        }
+        else 
+            yield return 0;
+    }
+
+    IEnumerator DropInBoard()
+    {
+        int targetRow = gameBoard.GetRowAvailableAt(currentCol);
+        if (targetRow >= 0)
+        {
+            isMoving = true;
+
+            float startY = transform.position.y;
+            float targetY = gameBoard.GetYAt(targetRow);
+
+            float t = 0;
+            while (t <= dropDuration)
+            {
+                t += Time.deltaTime;
+                float currentY = Mathf.Lerp(startY, targetY, t / dropDuration);
+                transform.position = new Vector3(transform.position.x, currentY, transform.position.z);
+
+                yield return null;
+            }
+
+            transform.position = new Vector3(transform.position.x, targetY, transform.position.z);
+            currentRow = targetRow;
+            gameBoard.PlaceDiscOn(color, currentRow, currentCol);
+
+            isMoving = false;
+
+            yield return StartCoroutine(gameBoard.Rotate());
+        }
+        else
+            yield return 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isAboveBoard)
+        {
+            if (currentRow == -1)
+            {
+                if (isPlayer && !isMoving)
+                {
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        touchDownPosition = Input.mousePosition;
+                    }
+                    else if (Input.GetMouseButtonUp(0))
+                    {
+                        touchUpPosition = Input.mousePosition;
+                        
+                        Vector3 swipeDirection = (touchUpPosition - touchDownPosition).normalized;
+                        
+                        if (swipeDirection.y > -0.5f)
+                        {
+                            if (Mathf.Abs(swipeDirection.x) >= 0.5)
+                            {
+                                if (swipeDirection.x > 0)
+                                    StartCoroutine(ShiftToColumn(currentCol + color));
+                                else if (swipeDirection.x < 0)
+                                    StartCoroutine(ShiftToColumn(currentCol - color));
+                            }
+                        }
+                        else
+                            StartCoroutine(DropInBoard());
+                    }
+                }
+            }
+        }
+    }
+}
